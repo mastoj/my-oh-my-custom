c4883f8011d91f0258a4534855bbe39961c015f5
Tomas Jansson <tomas.jansson@gmail.com>
2017-01-16 10:48:48 +0100

    added some docker aliases

--- plugins/tomas/tomas.plugin.zsh
+++ plugins/tomas/tomas.plugin.zsh
@@ -3,3 +3,9 @@ bindkey "[D" backward-word
 bindkey "[C" forward-word
 
 alias tomas='echo "tomas"'
+
+alias val-containers='docker ps -a'
+alias val-containers-rm='docker rm $(docker ps -a -q)'
+alias val-containers-stop='docker stop $(docker ps -a -q)'
+
+alias val-images-rm='docker rmi $(docker images -q)'
