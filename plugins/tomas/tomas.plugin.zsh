ad74f60a98de6c3173b07a7bbad94362067fb039
Tomas Jansson <tomas.jansson@gmail.com>
2017-01-31 14:54:47 +0100

    show & kill camera

--- plugins/tomas/tomas.plugin.zsh
+++ plugins/tomas/tomas.plugin.zsh
@@ -10,6 +10,9 @@ alias val-containers-stop='docker stop $(docker ps -a -q)'
 
 alias val-images-rm='docker rmi $(docker images -q)'
 
+alias cam-find='lsof | grep -i "VDC"'
+alias cam-kill='cam-find | awk -v N=2 '\''{print $N}'\'' | xargs kill'
+
 #color(){
 #    set -o pipefail;"$@" 2>&1>&3|sed $'s,.*,\e[31m&\e[m,'>&2 3>&1
 #}
