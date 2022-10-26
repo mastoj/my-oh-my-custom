98e8a760dc4ff0ef759bd23beb2bdd9e48429d54
Tomas Jansson <tomas.jansson@gmail.com>
2022-10-26 20:06:39 +0200

    Add symbolic links

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Setting up symbolic links
 ln -s /home/tomas/git/mastoj/my-oh-my-custom/gitconfig-work /home/tomas/.gitconfig-work
 ln -s /home/tomas/git/mastoj/my-oh-my-custom/gitconfig /home/tomas/.gitconfig
-ln -s /home/tomas/git/mastoj/my-oh-my-custom/zshrc_pop /home/tomas/git/mastoj/.zshrc
+ln -s /home/tomas/git/mastoj/my-oh-my-custom/zshrc_pop .zshrc
 
 ln -s /home/tomas/lib/dotnet-install.sh /home/tomas/bin/dotnet-install
